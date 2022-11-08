bb24fb174551f548a965a533fbeb6ecb9a3ffb6e
Geon_27 <yukgeon98527@gmail.com>
2022-11-08 17:43:44 +0900

    Initial Commit

---
+++ Daejeon_Toilet_Seogu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Daejeon_Toilet_Seogu
+//
+//  Created by 육건 on 2022/11/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
